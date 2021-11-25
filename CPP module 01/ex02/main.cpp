e955b3f719762f18d6332b4c72cc9075485ee9dd
Jiwon-Choi <jwchoi.py@gmail.com>
2021-11-25 20:22:09 +0900

    Add ex02

---
+++ CPP module 01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+    std::string string = "HI THIS IS BRAIN";
+
+    std::string* stringPTR = &string;
+    std::string& stringREF = string;
+
+    std::cout << "============ address ============" << std::endl;
+    std::cout << "string    : " << &string << std::endl
+              << "stringPTR : " << stringPTR << std::endl
+              << "stringREF : " << &stringREF << std::endl << std::endl;
+
+    std::cout << "============= value =============" << std::endl;
+    std::cout << "string    : " << string << std::endl
+              << "stringPTR : " << *stringPTR << std::endl
+              << "stringREF : " << stringREF << std::endl;
+    return (0);
+}
