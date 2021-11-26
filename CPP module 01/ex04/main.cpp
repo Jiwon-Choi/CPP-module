c9e893921947e33192cea5e7aa2e2fde8aacf711
Jiwon-Choi <jwchoi.py@gmail.com>
2021-11-26 16:58:26 +0900

    Add ex04

---
+++ CPP module 01/ex04/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cout << "[Error] ./replace [filename] [str1] [str2]" << std::endl;
+        return (-1);
+    }
+
+    std::string filename = argv[1];
+    std::string strOld = argv[2];
+    std::string strNew = argv[3];
+
+    std::ifstream fin(filename);
+    std::ofstream fout(filename + ".replace");
+
+    if (fin.fail()) {
+        std::cout << "[Error] No such file" << std::endl;
+        return (-1);
+    }
+
+    while (!fin.eof()) {
+        std::string line;
+        fin >> line;
+        std::string::size_type offset = 0;
+        while ((offset = line.find(strOld, offset)) != std::string::npos) {
+            fout << line.substr(0, offset);
+            line = line.substr(offset + strOld.length());
+            offset = 0;
+            fout << strNew;
+        }
+        fout << line << std::endl;
+    }
+
+    fin.close();
+    fout.close();
+    return (0);
+}
